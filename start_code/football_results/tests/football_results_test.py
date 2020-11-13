9853c79c07efb5d0cb8d1d082ca8bcb30008cc3b
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2020-11-13 22:26:38 +0200

    footbal finished

--- start_code/football_results/tests/football_results_test.py
+++ start_code/football_results/tests/football_results_test.py
@@ -2,14 +2,22 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
+    
     # Test we get the right result string for a final score dictionary representing -
-
+    def test_get_result(self):
+        home_win = {'home_score': 3, 'away_score': 0}
+        self.assertEqual('Home win', get_result(home_win))
         # Home win
         # Away win
         # Draw
 
     # Test we get right list of result strings for a list of final score dictionaries. 
+
+    def test_get_results(self):
+        home_win = {'home_score': 3, 'away_score': 0}
+        away_win = {'home_score': 0, 'away_score': 3}
+        draw = {'home_score': 0, 'away_score': 0}
+        self.assertEqual(['Home win', 'Away win', 'Draw'], get_results([home_win, away_win, draw]))
 
 
 if __name__ == "__main__":
