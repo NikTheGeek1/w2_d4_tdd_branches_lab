28fcc88a4a2c487177c769fd31b5f3027baf53d3
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2020-11-13 11:29:54 +0200

    compound_interest_final_tests

--- start_code/compound_interest/tests/compound_interest_test.py
+++ start_code/compound_interest/tests/compound_interest_test.py
@@ -32,8 +32,11 @@ class CompoundInterestTest(unittest.TestCase):
     def test5(self):
         self.assertEqual(118380.16, CompoundInterest(100, .05, 8, 1000).calculator_with_contributions())
     # Should return 156,093.99 given 100 principal, 5 percent, 10 years, 1000 per month
-    
+    def test6(self):
+        self.assertEqual(156093.99, CompoundInterest(100, 0.05, 10, 1000).calculator_with_contributions())
     # Should return 475,442.59 given 116028.86, 7.5 percent, 8 years, 2006 per month
-
+    def test7(self):
+        self.assertEqual(475442.59, CompoundInterest(116028.86, 0.075, 8, 2006).calculator_with_contributions())
     # Should return 718,335.96 given 116028.86 principal, 9 percent, 12 years, 1456 per month
-
+    def test8(self):
+        self.assertEqual(718335.97, CompoundInterest(116028.86, 0.09, 12, 1456).calculator_with_contributions())
