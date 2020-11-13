fcd473a13eca318691868d4fe235b085c2dc8163
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2020-11-13 14:33:17 +0200

    matrix finished

--- start_code/matrix/src/matrix.py
+++ start_code/matrix/src/matrix.py
@@ -1,9 +1,23 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
+        self.matrix = self.string_to_matrix(matrix_string)
+
+    def string_to_matrix(self, string):
+        matrix = []
+
+        for string in string.split('\n'):
+            row = []
+            for item in string.split(' '):
+                row.append(int(item))
+            matrix.append(row)
+        return matrix
 
     def row(self, index):
-        pass
+        return self.matrix[index - 1]
 
     def column(self, index):
-        pass
+        column = []
+        for row in self.matrix:
+            column.append(row[index - 1])
+        return column
--- start_code/matrix/tests/matrix_test.py
+++ start_code/matrix/tests/matrix_test.py
@@ -16,6 +16,9 @@ class MatrixTest(unittest.TestCase):
     #
     # 1 2
     # 10 20
+    def test_extract_given_row(self):
+        matrix = Matrix('1 2\n10 20')
+        self.assertEqual(matrix.row(2), [10, 20] )
 
     # Test can extract row from non-square matrix
     # Example matrix:
@@ -24,24 +27,34 @@ class MatrixTest(unittest.TestCase):
     # 4 5 6
     # 7 8 9
     # 8 7 6
-
+    def test_row_non_sq(self):
+        matrix = Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6')
+        self.assertEqual(matrix.row(2), [4, 5, 6])
+    
     # Test can extract a column
 
     # Test can extract column from a one number matrix
-
+    def test_column(self):
+        matrix = Matrix('1')
+        self.assertEqual(matrix.column(1), [1])
     # Test can extract a column from non-square matrix
     # Example matrix:
     #
     # 1 2 3 4
     # 5 6 8 8
     # 9 8 7 6
-
+    def test_extract_column_non_square(self):
+        matrix = Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6')
+        self.assertEqual(matrix.column(2), [2, 5, 8, 7])
     # Test can extract column when numbers have different number of digits
     # Example matrix:
     #
     # 89 1903 3
     # 18 3 1
     # 9 4 800
+    def test_extract_column_different_number(self):
+        matrix = Matrix('89 1903 3\n18 3 1\n9 4 800')
+        self.assertEqual(matrix.column(2), [1903, 3, 4])
 
 
 
