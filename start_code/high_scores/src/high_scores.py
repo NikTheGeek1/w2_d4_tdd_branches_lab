b660c69a04d8b0d4f0ec58033102657c157f00c3
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2020-11-13 14:55:45 +0200

    high_scores_passes

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -1,10 +1,21 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    top_three = []
+    for _ in range(3):
+        top_three.append(max(scores))
+        scores.remove(max(scores))
+        if len(scores) == 0:
+            break
+    return top_three
+        
+def ordered(scores):
+    scores.sort()
+    return scores
+
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import latest, personal_best, personal_top_three, ordered
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
@@ -8,18 +8,27 @@ from src.high_scores import latest, personal_best, personal_top_three
 class HighScoresTest(unittest.TestCase):
     
     # Tests
-
+    def setUp(self):
+        self.scores = [1, 2, 10, 30, 5]
     # Test latest score (the last thing in the list)
-
+    def test_latest_score(self):
+        self.assertEqual(5, latest(self.scores))
     # Test personal best (the highest score in the list)
-
+    def test_personal_best(self):
+        self.assertEqual(30, personal_best(self.scores))
     # Test top three from list of scores
-
+    def test_top_three(self):
+        self.assertEqual([30, 10, 5], personal_top_three(self.scores))
     # Test ordered from highest tp lowest
-
+    def test_ordered(self):
+        self.assertEqual([1, 2, 5, 10, 30], ordered(self.scores))
     # Test top three when there is a tie
-
+    def test_top_three_tie(self):
+        self.assertEqual([20, 20, 10], personal_top_three([10, 20, 20]))
     # Test top three when there are less than three
-
+    def test_top_three_less_than_three(self):
+        self.assertEqual([2, 1], personal_top_three([1, 2]))
     # Test top three when there is only one
+    def test_top_three_when_just_one(self):
+        self.assertEqual([1], personal_top_three([1]))
     
